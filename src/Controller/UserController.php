df40683200d55ff37751067ec382316d83595aac
Skyrolight <skyrolight@gmail.com>
2022-11-09 19:13:35 +0100

    merge files

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,11 +22,11 @@ class UserController
 
   public function promptInscription(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
   {
-    return $this->view->render($response, 'inscription.twig');
+    return $this->view->render($response, '/profile/signIn.html.twig');
     return $response; 
   }
 
-  public function inscription(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+  public function signIn(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
   {
     $args = $request->getParsedBody();
     $repository = $this->em->getRepository(User::class);
@@ -40,44 +40,44 @@ class UserController
     if (isset($args["mail"]) && isset($args["password"]) && isset($args["pseudo"])) {
       if ($args["password"] != $args["passwordVerif"]) {
         $erreurVerifMdp = "Les mots de passe ne correspondent pas";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twig', [
           'erreurVerifMdp' => $erreurVerifMdp,
           'products' => $productMail
         ]);
       }
       else if ($args["pseudo"] == "") {
         $erreurPseudo = "Veuillez rentrer un pseudo";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twig', [
           'erreurPseudo' => $erreurPseudo,
         ]);
       }
       else if ($args["password"] == "" ) {
         $erreurMdp = "Veuillez rentrer un mot de passe";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twig', [
           'erreurMdp' => $erreurMdp,
         ]);
       }
       else if ($args["passwordVerif"] == "") {
         $erreurVerifMdp = "Veuillez confirmez votre mot de passe";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twig', [
           'erreurVerifMdp' => $erreurVerifMdp
         ]);
       }
       else if ($args["mail"] == "") {
         $erreurMail = "Veuillez rentrer une adresse mail";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twig', [
           'erreurMail' => $erreurMail,
         ]);
       }
       else if ($args["mail"] = $productMail) {
       $erreurMail = "L'adresse mail existe deja veuillez changer";
-      return $this->view->render($response, 'inscription.twig', [
+      return $this->view->render($response, '/profile/signIn.html.twigg', [
         'erreurMail' => $erreurMail,
       ]);
       }
       else if ($args["pseudo"] = $productPseudo) {
         $erreurPseudo = "Le pseudo existe deja veuillez changer";
-        return $this->view->render($response, 'inscription.twig', [
+        return $this->view->render($response, '/profile/signIn.html.twigg', [
           'erreurPseudo' => $erreurPseudo
         ]);
       }

a83bb99c0fde93a1cc636c65784cbe7737d29465
Alban Ktz <alban.ktz@gmail.com>
2022-11-14 19:39:08 +0100

    a propos

--- public/index.php
+++ public/index.php
@@ -17,6 +17,8 @@ $app = AppFactory::create();
 $app->add(TwigMiddleware::createFromContainer($app));
 
 $app->get('/', \App\Controller\HomeController::class . ':home');
+$app->get('/a-propos', \App\Controller\HomeController::class . ':propos');
+
 $app->get('/gallery/{idGallery}', \App\Controller\ImagesController::class . ':images');
 $app->get('/new-gallery', \App\Controller\HomeController::class . ':createGalleryPage');
 $app->post('/new-gallery/create', \App\Controller\HomeController::class . ':createGalleryFunction');
